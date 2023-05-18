afdc84dbb09855f5458f258d684b44f5d449c3f6
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-05-18 17:00:23 +0300

    getting things done slowly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      this will be feedback
-    </div>
-  );
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return(
+      <>
+        <Section title="Please leave feedback">
+        </Section>
+        <Section title="Statistics">
+            <Statistics
+            good="0"
+            neutral="0"
+            bad="0"
+            total="0"
+            positivePercentage= "0"
+            />
+        </Section>
+      </>
+    )
+  }
 };
